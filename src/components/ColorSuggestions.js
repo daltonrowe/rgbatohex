137706d220cccfa937533555807396461844ab26
Dalton Rowe <cdaltonrowe@gmail.com>
2019-05-02 19:29:48 -0500

    stub suggestions and option

---
+++ src/components/ColorSuggestions.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "../styles/components/ColorInputOutput.scss"
+
+const ColorSuggestions = ({ props }) => (
+  <div className="color-converter__input_wrapper" />
+)
+
+export default ColorSuggestions
