925ecb3f42a6ff96af69362599fe0aa20f2077ad
Dalton Rowe <cdaltonrowe@gmail.com>
2019-05-02 20:38:17 -0500

    with bg

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `RGBA to Hex Converter`,
+        short_name: `rgbatohex`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -43,8 +43,6 @@ module.exports = {
         cookieDomain: "rgbatohex.com",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
