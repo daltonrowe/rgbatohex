2b9a7e9f6e0a011d9bc565a5d4269cb1fa85f06b
Dalton Rowe <cdaltonrowe@gmail.com>
2019-05-02 19:30:04 -0500

    github, description in header

--- src/layout/header.js
+++ src/layout/header.js
@@ -2,11 +2,17 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, github }) => (
   <header>
     <h1>
       <Link to="/">{siteTitle}</Link>
     </h1>
+    <span>
+      Also as Hex to RGBA converter.
+      <a target="_blank" rel="noopenner noreferrer">
+        View and contribute on Github!
+      </a>
+    </span>
   </header>
 )
 
